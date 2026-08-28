8ec302afd6ab0d62ad2a2c947ce4bfe67f968a7a
chatwithme02 <stargazing803@gmail.com>
2026-08-28 23:01:45 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,18 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"
+);
 
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js"
+);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCJbM803t7UFreQ2UL9K_fiN5b3GoQbhpU",
+  authDomain: "chat-with-me-c641b.firebaseapp.com",
+  projectId: "chat-with-me-c641b",
+  storageBucket: "chat-with-me-c641b.firebasestorage.app",
+  messagingSenderId: "61151214390",
+  appId: "1:61151214390:web:c2a41c8ff3a210f31f9197"
+});
+
+const messaging = firebase.messaging();
